a0e38ced355c34e5026ae203d21b0e0cdf86c8ad
Leandro López <inkel.ar@gmail.com>
2018-01-09 16:47:43 -0300

    Migrate Prometheus::Client::Registry from Ruby
    It is not threadsafe, and it's missing the auxiliary methods for
    creating different types of metrics.

---
+++ spec/prometheus/client/registry_spec.cr
@@ -0,0 +1,76 @@
+require "../../spec_helper"
+require "../../../src/prometheus/client/registry"
+
+def with_registry
+  yield Prometheus::Client::Registry.new
+end
+
+struct Metric
+  property name
+  def initialize(@name : Symbol)
+  end
+end
+
+describe Prometheus::Client::Registry do
+  describe ".new" do
+    it "returns a new registry instance" do
+      with_registry do |registry|
+        registry.should be_a(Prometheus::Client::Registry)
+      end
+    end
+  end
+
+  describe "#register" do
+    it "registers a new metric container and returns it" do
+      metric = Metric.new(:test)
+
+      with_registry do |registry|
+        registry.register(metric).should eq(metric)
+      end
+    end
+
+    it "raises an exception if a metric name gets registered twice" do
+      metric = Metric.new(:test)
+
+      with_registry do |registry|
+        registry.register(metric)
+
+        expect_raises(Prometheus::Client::Registry::AlreadyRegisteredError) do
+          registry.register(metric)
+        end
+      end
+    end
+  end
+
+  describe "#exist?" do
+    it "returns true if a metric name has been registered" do
+      with_registry do |registry|
+        registry.register(Metric.new(name: :test))
+
+        registry.exist?(:test).should eq(true)
+      end
+    end
+
+    it "returns false if a metric name has not been registered yet" do
+      with_registry do |registry|
+        registry.exist?(:test).should eq(false)
+      end
+    end
+  end
+
+  describe "#get" do
+    it "returns a previously registered metric container" do
+      with_registry do |registry|
+        registry.register(Metric.new(name: :test))
+
+        registry.get(:test).should eq(:test)
+      end
+    end
+
+    it "returns nil if the metric has not been registered yet" do
+      with_registry do |registry|
+        registry.get(:test).should eq(nil)
+      end
+    end
+  end
+end
--- src/prometheus/client/registry.cr
+++ src/prometheus/client/registry.cr
@@ -1,6 +1,31 @@
 module Prometheus
   module Client
     class Registry
+
+      class AlreadyRegisteredError < Exception
+      end
+
+      def initialize
+        @metrics = {} of Symbol => Symbol
+      end
+
+      def register(metric)
+        name = metric.name
+
+        raise AlreadyRegisteredError.new("#{name} has already been registered") if exist?(name)
+
+        @metrics[name] = name
+
+        metric
+      end
+
+      def exist?(name : Symbol)
+        @metrics.has_key?(name)
+      end
+
+      def get(name : Symbol)
+        @metrics[name]?
+      end
     end
   end
 end
