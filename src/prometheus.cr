621617cf58be91476fa34d86f38e42f61e915a87
Leandro López <inkel.ar@gmail.com>
2018-01-09 15:26:36 -0300

    Initial commit

---
+++ src/prometheus.cr
@@ -0,0 +1,6 @@
+require "./prometheus/*"
+
+# TODO: Write documentation for `Prometheus::Crystal::Client`
+module Prometheus::Crystal::Client
+  # TODO: Put your code here
+end
---
+++ src/prometheus/version.cr
@@ -0,0 +1,3 @@
+module Prometheus::Crystal::Client
+  VERSION = "0.1.0"
+end
