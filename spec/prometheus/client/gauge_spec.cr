4d50c20ce1a3d15b7020aa8a18cb5336fc7fd996
Leandro López <inkel.ar@gmail.com>
2018-01-10 11:49:06 -0300

    Migrate Prometheus::Client::Gauge from Ruby

---
+++ spec/prometheus/client/gauge_spec.cr
@@ -0,0 +1,77 @@
+require "../../spec_helper"
+require "../../../src/prometheus/client/gauge"
+
+def with_gauge
+  yield Prometheus::Client::Gauge.new(:test, "some docstring", {:label => "value"})
+end
+
+describe Prometheus::Client::Gauge do
+  describe ".new" do
+    it "defaults value to 0.0" do
+      with_gauge do |gauge|
+        gauge.get.should eq(0.0)
+      end
+    end
+  end
+
+  describe "#set" do
+    it "sets a metric value" do
+      with_gauge do |gauge|
+        gauge.set({} of Symbol => String, 42.0)
+        gauge.get.should eq(42.0)
+      end
+    end
+
+    it "sets a metric value for a given label set" do
+      with_gauge do |gauge|
+        gauge.set({ :test => "value" }, 5.0)
+        gauge.get({ :test => "value" }).should eq(5.0)
+        gauge.get.should eq(0.0)
+      end
+    end
+  end
+
+  describe "#increment" do
+    it "increments the gauge" do
+      with_gauge do |gauge|
+        gauge.increment
+        gauge.get.should eq(1.0)
+      end
+    end
+
+    it "increments the gauge by a given value" do
+      with_gauge do |gauge|
+        gauge.increment({} of Symbol => String, 5.0)
+        gauge.get.should eq(5.0)
+      end
+    end
+
+    it "returns the new gauge value" do
+      with_gauge do |gauge|
+        gauge.increment.should eq(1.0)
+      end
+    end
+  end
+
+  describe "#decrement" do
+    it "decrements the gauge" do
+      with_gauge do |gauge|
+        gauge.decrement
+        gauge.get.should eq(-1.0)
+      end
+    end
+
+    it "decrements the gauge by a given value" do
+      with_gauge do |gauge|
+        gauge.decrement({} of Symbol => String, 5.0)
+        gauge.get.should eq(-5.0)
+      end
+    end
+
+    it "returns the new gauge value" do
+      with_gauge do |gauge|
+        gauge.decrement.should eq(-1.0)
+      end
+    end
+  end
+end
---
+++ src/prometheus/client/gauge.cr
@@ -0,0 +1,19 @@
+require "./metric"
+
+module Prometheus
+  module Client
+    class Gauge < Metric
+      def set(labels = {} of Symbol => String, value : Float64 = 0.0)
+        @values[labels] = value
+      end
+
+      def increment(labels = {} of Symbol => String, by : Float64 = 1.0)
+        @values[labels] += by
+      end
+
+      def decrement(labels = {} of Symbol => String, by : Float64 = 1.0)
+        @values[labels] -= by
+      end
+    end
+  end
+end
